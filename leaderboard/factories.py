6b63147ee23aa452e468b22c32467970e7d6614c
Ankit Rastogi <5370144+ankitrks@users.noreply.github.com>
2024-07-09 17:16:58 +0530

    Database settings updated

--- leaderboard/factories.py
+++ leaderboard/factories.py
@@ -10,6 +10,6 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     name = factory.LazyAttribute(lambda _: fake.name())
     age = factory.LazyAttribute(lambda _: fake.random_int(min=18, max=70))
-    points = 0
+    points = factory.LazyAttribute(lambda _: fake.random_int(min=2, max=67))
     address = factory.LazyAttribute(lambda _: fake.address())
     # photo = factory.LazyAttribute(lambda _: fake.image_url())
